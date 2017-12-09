1d64593251ada5cfc2ace4d7e2d8327573c69367
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2017-12-09 18:59:02 +0900

    まよった

--- q-learn/test_sample_1.py
+++ q-learn/test_sample_1.py
@@ -8,6 +8,8 @@ import numpy as np
 import time
 from dummy import *
 
+num_dizitized = 6  #number of digitized/devided
+
 # [1]define Q-function
 # convert observed situation to discrete values
 def bins(clip_min, clip_max, num):
@@ -26,14 +28,14 @@ def digitize_state(observation):
     return sum([x * (num_dizitized**i) for i, x in enumerate(digitized)])
 
 # [2]function which determine action a(t)
-def get_action(next_state, episode, action):
+def get_action(q_table, next_state, episode, action):
     #epsilon-greedy
     epsilon = 0.5 * (1 / (episode + 1))
     if epsilon <= np.random.uniform(0, 1):
         next_action = np.argmax(q_table[next_state])
     else:
         #next_action = np.random.choice([0, 1])
-        next_action = np.randint(action.size)
+        next_action = randint(action.size)
     return next_action
 
 # [3]function which update Q-table
@@ -46,69 +48,74 @@ def update_Qtable(q_table, state, action, reward, next_state):
 
     return q_table
 
-# [4] start main function. set parameters
-action_array=np.array([0.1,0.2,0.3])
-max_number_of_steps = 100  #number of steps for 1 trial
-num_consecutive_iterations = 100  #mean of number of trial to use for evaluation of finish of learning
-num_episodes = 1000  #number of all trials
-goal_average_reward = 50  #boder line of rewards to stop learning
-# state is digitized/devided into 6 parts (there is 4 variables) and making q-table (as q-function)
-num_dizitized = 6  #number of digitized/devided
-q_table = np.random.uniform(    low=-1, high=1, size=(num_dizitized**4, action_array.size))
+def q_learning(sensor_array):
+    # [4] start main function. set parameters
+    action_array=np.array([0.1,0.2,0.3])
+    max_number_of_steps = 100  #number of steps for 1 trial
+    num_consecutive_iterations = 100  #mean of number of trial to use for evaluation of finish of learning
+    num_episodes = 1000  #number of all trials
+    goal_average_reward = 50  #boder line of rewards to stop learning
+    # state is digitized/devided into 6 parts (there is 4 variables) and making q-table (as q-function)
+    #q_table = np.random.uniform(low=-1, high=1, size=(num_dizitized**4, action_array.size))
+    q_table = np.random.uniform(low=-1, high=1, size=4)
+    #４じすぷらいんほかんてきな
 
-total_reward_vec = np.zeros(num_consecutive_iterations)  #contains rewards of each trial
-final_x = np.zeros((num_episodes, 1))  #contains a value of x (t=200) after learning
-islearned = 0  #flg of finishing learning
-isrender = 0  #flg of drawing
+    total_reward_vec = np.zeros(num_consecutive_iterations)  #contains rewards of each trial
+    final_x = np.zeros((num_episodes, 1))  #contains a value of x (t=200) after learning
+    islearned = 0  #flg of finishing learning
+    isrender = 0  #flg of drawing
 
-# [5] main tourine
-sensor_array=np.array([1,0.5,0.2])
-list_weight=np.array([1,2,3])
-for episode in range(num_episodes):  #repeat for number of trials
-    # initialize enviroment
-    observation = sensor_input_dummy(sensor_array)
-    #observation = env.reset()
-    state = digitize_state(observation)
-    #TODO
-    action = np.argmax(q_table[state])
-    episode_reward = 0
-
-    for t in range(max_number_of_steps):  #roup for 1 trial
-        # calcurate s_{t+1}, r_{t} etc based on selected/conducted action
+    # [5] main tourine
+    #sensor_array=np.array([1,0.5,0.2])
+    list_weight=np.array([1,2,3])
+    for episode in range(num_episodes):  #repeat for number of trials
+        # initialize enviroment
         observation = sensor_input_dummy(sensor_array)
-        facial_expression = facial_input_dummy()
-        reward = calc_reward_dummy(facial_expression)
-        done=done_dummy()
+        state = digitize_state(observation)
+        #TODO
+        action = np.argmax(q_table[state])
+        episode_reward = 0
 
-        episode_reward += reward  #報酬を追加
+        for t in range(max_number_of_steps):  #roup for 1 trial
+            # calcurate s_{t+1}, r_{t} etc based on selected/conducted action
+            observation = sensor_input_dummy(sensor_array)
+            facial_expression = facial_input_dummy()
+            reward = calc_reward_dummy(facial_expression)
+            done=done_dummy()
 
-        # calcurate s_{t+1} and update q-table(as q-function)
-        next_state = digitize_state(observation)  #convert s_{t+1} to digitized value
-        q_table = update_Qtable(q_table, state, action, reward, next_state)
+            episode_reward += reward  #報酬を追加
 
-        # evaluate the next action a_{t+1}
-        action = get_action(next_state, episode, action)    # a_{t+1} 
+            # calcurate s_{t+1} and update q-table(as q-function)
+            next_state = digitize_state(observation)  #convert s_{t+1} to digitized value
+            q_table = update_Qtable(q_table, state, action, reward, next_state)
 
-        state = next_state
+            # evaluate the next action a_{t+1}
+            action = get_action(q_table, next_state, episode, action)    # a_{t+1} 
+            state = next_state
 
-        # processing of end
-        if done:
-            print('%d Episode finished after %f time steps / mean %f' %
-                  (episode, t + 1, total_reward_vec.mean()))
-            total_reward_vec = np.hstack((total_reward_vec[1:],
-                                          episode_reward))  #record a reward
-            if islearned == 1:  #if learning has finished
-                final_x[episode, 0] = observation[0] #contain the value of final x
-            break
+            # processing of end
+            if done:
+                print('%d Episode finished after %f time steps / mean %f' %
+                      (episode, t + 1, total_reward_vec.mean()))
+                total_reward_vec = np.hstack((total_reward_vec[1:],
+                                              episode_reward))  #record a reward
+                if islearned == 1:  #if learning has finished
+                    final_x[episode, 0] = observation[0] #contain the value of final x
+                break
 
-    if (total_reward_vec.mean() >=
-            goal_average_reward):  # if the rewards of the most recent 100 episodes is larger than normal rewards, success 
-        print('Episode %d train agent successfuly!' % episode)
-        islearned = 1
-        #np.savetxt('learned_Q_table.csv',q_table, delimiter=",") #if save Q-table
-        if isrender == 0:
-            #env = wrappers.Monitor(env, './movie/cartpole-experiment-1') #if save the movie
-            isrender = 1
+        if (total_reward_vec.mean() >=
+                goal_average_reward):  # if the rewards of the most recent 100 episodes is larger than normal rewards, success 
+            #print('Episode %d train agent successfuly!' % episode)
+            print('action',action)
+            islearned = 1
+            #np.savetxt('learned_Q_table.csv',q_table, delimiter=",") #if save Q-table
+            if isrender == 0:
+                #env = wrappers.Monitor(env, './movie/cartpole-experiment-1') #if save the movie
+                isrender = 1
 
-if islearned:
-    np.savetxt('final_x.csv', final_x, delimiter=",")
+    if islearned:
+        np.savetxt('final_x.csv', final_x, delimiter=",")
+
+if __name__ == '__main__':
+    sensor_array=np.array([1,0.5,0.2])
+    q_learning(sensor_array)
