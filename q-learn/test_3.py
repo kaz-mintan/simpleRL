71e464ede66a1fdda8607325eef015b011f361c3
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2017-12-19 09:21:26 +0900

    changed Q-table for LAS

--- q-learn/test_3.py
+++ q-learn/test_3.py
@@ -11,22 +11,22 @@ import time
 
 # [2]function which determine action a(t)
 #def get_action(next_state, episode):
-def get_action(next_state, episode):
+def get_action(next_state):
     #epsilon-greedy
     #epsilon = 0.5 * (1 / (episode + 1))
-    epsilon = 0.5 * (1 / (episode + 1))
+    epsilon = 0.5
     if epsilon <= np.random.uniform(0, 1):
         next_action = np.argmax(q_table[next_state])
     else:
         next_action = np.random.choice([0, 1])
     return next_action
 
-
 # [3]function which update Q-table
 def update_Qtable(q_table, state, action, reward, next_state):
     gamma = 0.99
     alpha = 0.5
-    next_Max_Q=max(q_table[next_state][0],q_table[next_state][1] )
+    #next_Max_Q=max(q_table[next_state][0],q_table[next_state][1] )
+    next_Max_Q=np.argmax(q_table[next_state])
     q_table[state, action] = (1 - alpha) * q_table[state, action] +\
             alpha * (reward + gamma * next_Max_Q)
 
@@ -34,57 +34,41 @@ def update_Qtable(q_table, state, action, reward, next_state):
 
 
 # [4] start main function. set parameters
-env = gym.make('CartPole-v0')
-max_number_of_steps = 200  #number of steps for 1 trial
-num_consecutive_iterations = 100  #mean of number of trial to use for evaluation of finish of learning
+#max_number_of_steps = 200  #number of steps for 1 trial
+max_number_of_steps = 200  #number of time window
+#num_consecutive_iterations = 100  #mean of number of trial to use for evaluation of finish of learning
 num_episodes = 2000  #number of all trials
-goal_average_reward = 195  #boder line of rewards to stop learning
-# state is digitized/devided into 6 parts (there is 4 variables) and making q-table (as q-function)
-num_dizitized = 6  #number of digitized/devided
+#goal_average_reward = 195  #boder line of rewards to stop learning
+num_action = 60 #deg
+num_face = 100 #%
+num_ir = 100 #mm
 q_table = np.random.uniform(
-    low=-1, high=1, size=(num_dizitized**4, env.action_space.n))
+    low=0, high=1,
+    size=(num_face*5+num_ir, num_action))
 
-total_reward_vec = np.zeros(num_consecutive_iterations)  #contains rewards of each trial
-final_x = np.zeros((num_episodes, 1))  #contains a value of x (t=200) after learning
-islearned = 0  #flg of finishing learning
-isrender = 0  #flg of drawing
-
+#total_reward_vec = np.zeros(num_consecutive_iterations)  #contains rewards of each trial
+#final_x = np.zeros((num_episodes, 1))  #contains a value of x (t=200) after learning
+#islearned = 0  #flg of finishing learning
+#isrender = 0  #flg of drawing
 
 # [5] main tourine
 for episode in range(num_episodes):  #repeat for number of trials
     # initialize enviroment
-    observation = env.reset()
-    state = digitize_state(observation)
+    #state = digitize_state(observation)
+    state = np.hstack((get_face(), get_ir()))#TODO#return 
     action = np.argmax(q_table[state])
-    episode_reward = 0
 
-    for t in range(max_number_of_steps):  #roup for 1 trial
-        if islearned == 1:  #draw cartPole after learning
-            env.render()
-            time.sleep(0.1)
-            print (observation[0])  #outputs x of cart
+    for t in range(max_number_of_steps):  #roup for 1 time window
+        next_state = np.hstack((get_face(),get_ir()))
 
         # calcurate s_{t+1}, r_{t} etc based on selected/conducted action
-        observation, reward, done, info = env.step(action)
-
-        # set and give a reward
-        # this part could be a continus function
-        if done:
-            if t < 195:
-                reward = -200  #こけたら罰則
-            else:
-                reward = 1  #立ったまま終了時は罰則はなし
-        else:
-            reward = 1  #各ステップで立ってたら報酬追加
-
-        episode_reward += reward  #報酬を追加
+        reward = calc_reward(next_state) #TODO how to calc?
 
         # calcurate s_{t+1} and update q-table(as q-function)
-        next_state = digitize_state(observation)  #convert s_{t+1} to digitized value
         q_table = update_Qtable(q_table, state, action, reward, next_state)
 
         # evaluate the next action a_{t+1}
-        action = get_action(next_state, episode)    # a_{t+1} 
+        action = get_action(next_state)    # a_{t+1} 
 
         state = next_state
 
@@ -106,12 +90,5 @@ for episode in range(num_episodes):  #repeat for number of trials
         if isrender == 0:
             #env = wrappers.Monitor(env, './movie/cartpole-experiment-1') #if save the movie
             isrender = 1
-    #10エピソードだけでどんな挙動になるのか見たかったら、以下のコメントを外す
-    #if episode>10:
-    #    if isrender == 0:
-    #        env = wrappers.Monitor(env, './movie/cartpole-experiment-1') #動画保存する場合
-    #        isrender = 1
-    #    islearned=1;
 
-if islearned:
-    np.savetxt('final_x.csv', final_x, delimiter=",")
+    #np.savetxt('final_x.csv', final_x, delimiter=",")
