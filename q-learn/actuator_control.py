26f858bb0a8e0f590cd2f6387e2aa84cd3df09fa
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2017-12-20 06:44:33 +0900

    add sma mapping (theta vs pwm) and trajectory

--- q-learn/actuator_control.py
+++ q-learn/actuator_control.py
@@ -2,6 +2,17 @@
 import numpy as np
 import math
 
+def pwm2theta(pwm):
+    thre_a= 25
+    thre_b= 50
+    if pwm < thre_a:
+        theta = 0
+    elif pwm < thre_b:
+        theta = pwm - 25.0
+    else:
+        theta = pwm * 1.5 - 50.0
+    return theta
+
 def sim_traj(pwm, time_range):
     rate = np.array([1,2])
 
@@ -15,10 +26,9 @@ def sim_traj(pwm, time_range):
             sigma = sig
         else:
             sigma = sig*rate[1]
-        trajectory[t]=math.exp(-pow((t-mu),2)/(2*pow(sigma,2)))
+        trajectory[t]=pwm2theta(pwm)*math.exp(-pow((t-mu),2)/(2*pow(sigma,2)))
         print(t,trajectory[t])
     return trajectory
-
 
 traj=sim_traj(50,30)
 np.savetxt('test_traj.csv',traj,delimiter=',')
