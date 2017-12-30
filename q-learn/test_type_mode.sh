35645db5b3352fb20c73cf4614de9688c51ed464
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2017-12-30 19:52:43 +0900

    test script: modified to update random csv file's name

---
+++ q-learn/test_type_mode.sh
@@ -0,0 +1,49 @@
+python test_dqn.py happy posi delta
+mv action_pwm.csv action_pwm_happy_posi_delta.csv
+mv reward_seq.csv reward_seq_happy_posi_delta.csv
+mv situation.csv situation_happy_posi_delta.csv
+mv random_counter.csv random_happy_posi_delta.csv
+
+
+python test_dqn.py happy nega delta
+mv action_pwm.csv action_pwm_happy_nega_delta.csv
+mv reward_seq.csv reward_seq_happy_nega_delta.csv
+mv situation.csv situation_happy_nega_delta.csv
+mv random_counter.csv random_happy_nega_delta.csv
+
+python test_dqn.py sad posi delta
+mv action_pwm.csv action_pwm_sad_posi_delta.csv
+mv reward_seq.csv reward_seq_sad_posi_delta.csv
+mv situation.csv situation_sad_posi_delta.csv
+mv random_counter.csv random_sad_posi_delta.csv
+
+python test_dqn.py sad nega delta
+mv action_pwm.csv action_pwm_sad_nega_delta.csv
+mv reward_seq.csv reward_seq_sad_nega_delta.csv
+mv situation.csv situation_sad_nega_delta.csv
+mv random_counter.csv random_sad_nega_delta.csv
+
+
+python test_dqn.py happy posi heuristic
+mv action_pwm.csv action_pwm_happy_posi_heuristic.csv
+mv reward_seq.csv reward_seq_happy_posi_heuristic.csv
+mv situation.csv situation_happy_posi_heuristic.csv
+mv random_counter.csv random_happy_posi_heuristic.csv
+
+python test_dqn.py happy nega heuristic
+mv action_pwm.csv action_pwm_happy_nega_heuristic.csv
+mv reward_seq.csv reward_seq_happy_nega_heuristic.csv
+mv situation.csv situation_happy_nega_heuristic.csv
+mv random_counter.csv random_happy_nega_heuristic.csv
+
+python test_dqn.py sad posi heuristic
+mv action_pwm.csv action_pwm_sad_posi_heuristic.csv
+mv reward_seq.csv reward_seq_sad_posi_heuristic.csv
+mv situation.csv situation_sad_posi_heuristic.csv
+mv random_counter.csv random_sad_posi_heuristic.csv
+
+python test_dqn.py sad nega heuristic
+mv action_pwm.csv action_pwm_sad_nega_heuristic.csv
+mv reward_seq.csv reward_seq_sad_nega_heuristic.csv
+mv situation.csv situation_sad_nega_heuristic.csv
+mv random_counter.csv random_sad_nega_heuristic.csv
